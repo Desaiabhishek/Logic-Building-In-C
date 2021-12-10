31e3009d50f76d23e36048a35f820fe7c3af8162
Desaiabhishek <desaiabhi2019@gmail.com>
2021-12-10 14:11:49 +0530

    Logic Building in c

---
+++ Assignment_01/2.Program to Print 5 Times India On Screen.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display()
+{
+    int i = 0;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("\n India");
+    }
+}
+
+int main()
+{
+    Display();
+
+    getch();
+    return 0;
+}
