a15137c75ea5e793c1df951d767d55d362d86eb3
Desaiabhishek <desaiabhi2019@gmail.com>
2021-12-22 18:24:15 +0530

    Logic Building In C

---
+++ Assignment_07/04.Accept N From User And Printf All Odd Numbers Up to N.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void OddDisplay(int iNo)
+{
+  int i = 0;
+
+  for (i = 1; i <= iNo; i++)
+  {
+      if( i % 2 != 0)
+      {
+          printf(" %d ",i);
+      }
+  }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("\n Enter A Number =>");
+    scanf("%d",&iValue);
+
+    OddDisplay(iValue);
+
+    getch();
+    return 0;
+
+}
