29871a6c271147e8e4a68c0e72ea1ea9dac32b51
Desaiabhishek <desaiabhi2019@gmail.com>
2021-12-23 20:13:15 +0530

    Logic Building In C

---
+++ Assignment_08/05.Accept Number From User And Display its Table In Reverse Order.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void TableRev(int iNo)
+{
+    int i = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(i = 10; i >= 1; i--)
+    {
+        printf(" %d ",iNo * i);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("\n Enter A Number =>");
+    scanf("%d",&iValue);
+
+    TableRev(iValue);
+
+    getch();
+    return 0;
+}
