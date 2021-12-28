53044d3eba7b27b762b9d68af247b727a3869ba6
Desaiabhishek <desaiabhi2019@gmail.com>
2021-12-28 20:14:09 +0530

    Logic Building In C

---
+++ Assignment_12/03.Accept Number From User And Count Frequency of 2 in it.c
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int CountTwo(int iNo)
+{
+  int i = 0, Num = 0;
+
+  while(iNo != 0)
+  {
+      Num = iNo %10;
+
+      if(Num == 2)
+      {
+          i++;
+      }
+      iNo = iNo/10;
+  }
+  return i;
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("\n Enter A Number =");
+    scanf("%d",&iValue);
+
+    printf("\n Two Count in Digit is %d",CountTwo(iValue));
+
+    getch();
+    return 0;
+}
