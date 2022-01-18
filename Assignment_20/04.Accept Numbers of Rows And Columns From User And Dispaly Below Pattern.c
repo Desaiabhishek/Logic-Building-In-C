337a5e1cd436f05b39bec15542bd724264ee0ae0
Desaiabhishek <desaiabhi2019@gmail.com>
2022-01-18 22:20:30 +0530

    Logic Building in C

---
+++ Assignment_20/04.Accept Numbers of Rows And Columns From User And Dispaly Below Pattern.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == 1|| j == 1 || i + j == iCol+1 || i == iRow || j == iCol)
+            {
+                printf(" * ");
+            }
+            else if(i+j < iCol+1)
+            {
+                printf(" # ");
+            }
+            else
+            {
+                printf(" $ ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("\n Enter A Row Numbers =");
+    scanf("%d",&iValue1);
+
+    printf("\n Enter A Column Numbers =");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    getch();
+    return 0;
+}
